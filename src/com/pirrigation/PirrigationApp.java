eda04ddda9a77468adc8e8be998dfe818e5edeb9
r4dx <r4dx@bk.ru>
2016-04-30 18:23:56 +0300

    Initial commit

---
+++ src/com/pirrigation/PirrigationApp.java
@@ -0,0 +1,13 @@
+package com.pirrigation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PirrigationApp {
+    private static final Logger logger = LoggerFactory.getLogger(PirrigationApp.class);
+
+    public static void main(String[] args) {
+        logger.info("Ping");
+    }
+
+}
