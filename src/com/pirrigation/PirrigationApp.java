6c23b2cf633e78e847bc91cfdf0bd2d28e88e7e0
r4dx <r4dx@bk.ru>
2016-04-30 23:09:08 +0300

    Done

--- src/com/pirrigation/PirrigationApp.java
+++ src/com/pirrigation/PirrigationApp.java
@@ -1,5 +1,6 @@
 package com.pirrigation;
 
+import com.pi4j.io.gpio.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +8,22 @@ public class PirrigationApp {
     private static final Logger logger = LoggerFactory.getLogger(PirrigationApp.class);
 
     public static void main(String[] args) {
-        logger.info("Ping");
-    }
 
+        final Pin port =  RaspiPin.GPIO_25;
+        final int seconds = 5;
+
+        logger.info("Enabling {} for {} seconds", port, seconds);
+
+        final GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(port);
+        pin.high();
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            logger.error("Interrupted while waiting", e);
+        }
+        pin.low();
+
+        gpio.shutdown();
+    }
 }
