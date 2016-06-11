738bbba07c169946c59c64b1a436c61156c2f663
r4dx <r4dx@bk.ru>
2016-06-11 22:02:32 +0300

    Logging in controller (instead of relying on access.log)

--- src/com/pirrigation/http/controller/HttpPumpController.java
+++ src/com/pirrigation/http/controller/HttpPumpController.java
@@ -2,6 +2,8 @@ package com.pirrigation.http.controller;
 
 import com.pirrigation.http.controller.responses.PumpPostResponse;
 import com.pirrigation.water.Pump;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,8 @@ import java.time.Duration;
 
 @RestController
 public class HttpPumpController {
+    private static final Logger logger = LoggerFactory.getLogger(HttpPumpController.class);
+
     private final Pump pump;
 
     public HttpPumpController(Pump pump) {
@@ -18,6 +22,7 @@ public class HttpPumpController {
 
     @RequestMapping(value = "/pump", method = RequestMethod.POST)
     public PumpPostResponse startPump(@RequestParam(value = "duration") Duration duration) {
+        logger.info("/pump POST: {}", duration);
         pump.start(duration);
         return new PumpPostResponse(duration);
     }
