694f7269162af94d7df118176ae295dc112047a0
r4dx <r4dx@bk.ru>
2016-06-11 21:54:26 +0300

    Moved logging to spring boot

--- src/com/pirrigation/PirrigationApp.java
+++ src/com/pirrigation/PirrigationApp.java
@@ -1,10 +1,5 @@
 package com.pirrigation;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.joran.spi.JoranException;
-import ch.qos.logback.core.util.StatusPrinter;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,24 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class PirrigationApp {
     public static void main(String[] args) {
-        initLogback();
         ConfigurableApplicationContext context = SpringApplication.run(PirrigationApp.class);
         PirrigationService service = context.getBean(PirrigationService.class);
         service.serve();
-    }
-
-    private static void initLogback() {
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        try {
-            JoranConfigurator configurator = new JoranConfigurator();
-            configurator.setContext(context);
-
-            context.reset();
-            configurator.doConfigure("conf/logback.xml");
-        } catch (JoranException je) {
-            // Will be caught in printInCaseOfErrorsOrWarnings but still just in case.
-            System.err.println("Error reading logback.xml");
-        }
-        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
     }
 }
--- src/com/pirrigation/factory/PumpFactory.java
+++ src/com/pirrigation/factory/PumpFactory.java
@@ -1,8 +1,10 @@
 package com.pirrigation.factory;
 
 import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
 import com.pirrigation.config.PumpConfig;
 import com.pirrigation.scheduler.Sleeper;
+import com.pirrigation.water.PiPump;
 import com.pirrigation.water.Pump;
 import com.pirrigation.water.StubPump;
 import org.mockito.Mockito;
@@ -18,19 +20,17 @@ public class PumpFactory {
     @Bean
     @Scope("singleton")
     public Pump pump(Sleeper sleeper, PumpConfig config, GpioController controller) {
-        return new StubPump(sleeper);
-        //return new PiPump(controller, config.getControlPin(), sleeper);
+        //return new StubPump(sleeper);
+        return new PiPump(controller, config.getControlPin(), sleeper);
     }
 
     @Bean
-    @Scope("singleton")
     public GpioController gpioController() {
-        return Mockito.mock(GpioController.class);
-        //return GpioFactory.getInstance();
+        //return Mockito.mock(GpioController.class);
+        return GpioFactory.getInstance();
     }
 
     @Bean
-    @Scope("singleton")
     public Sleeper sleeper() {
         return Sleeper.DEFAULT;
     }
